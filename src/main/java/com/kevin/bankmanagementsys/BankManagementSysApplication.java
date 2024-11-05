e468c68e1d9b861423341e4df340301f8c8bcbc5
emmTGB <79254221+emmTGB@users.noreply.github.com>
2024-11-05 14:35:00 +0800

    create repository

---
+++ src/main/java/com/kevin/bankmanagementsys/BankManagementSysApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.bankmanagementsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagementSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankManagementSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kevin/bankmanagementsys/BankManagementSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.bankmanagementsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
